6bffa1179b68647166cd9a3117a7d4f8040b9f47
Marcos Gronda <marcosgronda@gmail.com>
2023-08-09 14:49:28 -0300

    feat: graphing ex1.1

--- Exercises/ej1.py
+++ Exercises/ej1.py
@@ -1,8 +1,12 @@
 import json
 import os
 import csv
+import pandas as pd
+import plotly.express as px
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect, Pokemon
+
+ITERATIONS = 10000
 
 
 def ex1(configFileName):
@@ -20,19 +24,25 @@ def ex1(configFileName):
         with open(f"Results/Ex1/{ball}-data.csv", "w") as f:
             for pokemon in pokemons:
                 catches = 0
-                for _ in range(100):
+                for _ in range(ITERATIONS):
                     catched, probability = attempt_catch(pokemon, ball)
                     if catched:
                         catches += 1
-                print(f"{pokemon.name},{catches / 100}", file=f)
-
+                print(f"{pokemon.name},{catches / ITERATIONS}", file=f)
 
 def graph_ex1():
-    pokeballs = []
+    data = []
+
     for name in os.listdir("./Results/Ex1/"):
         pokeball = name.split("-")[0]
-        pokeballs.append(pokeball)
-        with open(f"{pokeball}-data.csv", "r") as pokeball_file:
+
+        with open(f"./Results/Ex1/{pokeball}-data.csv", "r") as pokeball_file:
             reader = csv.reader(pokeball_file)
-            
+
+            for pokemonName, probability in reader:
+                data.append({'Pokeball': pokeball, 'Pokemon': pokemonName, 'Probability': float(probability)})
+
+    df = pd.DataFrame(data)
+    fig = px.bar(df, x='Pokeball', y='Probability', color='Pokemon', barmode='group')
+    fig.show()
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Exercises.ej1 import ej1
+from Exercises.ej1 import ex1, graph_ex1
 
 if __name__ != "__main__":
     quit()
@@ -8,7 +8,7 @@ if __name__ != "__main__":
 # Exporta resultados a Results/Ex1
 
 ex1("./src/configs/item1.json")
-
+graph_ex1()
 
 
 
